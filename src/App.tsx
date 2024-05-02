6a9ba55761db65ed531bfa11dce108c4a41acee2
devsooki <devsooki@gmail.com>
2024-05-02 19:58:01 +0900

    feat: 기본 지도 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GlobalStyles from './style/globalStyles';
+import { HomePage } from 'pages';
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <HomePage />
     </>
   );
 }
---
+++ src/components/home/index.tsx
@@ -0,0 +1,12 @@
+import { Map } from 'assets';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <Map />
+    </>
+  )
+}
+
+export default Home;
